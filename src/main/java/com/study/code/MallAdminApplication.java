8991ccad6c1af53d0320ae6c65eb195fc2bcb582
suiweidong <7334501@qq.com>
2021-06-01 16:28:20 +0800

    引入nacos服务发现，及nacos的config

--- src/main/java/com/study/code/MallAdminApplication.java
+++ src/main/java/com/study/code/MallAdminApplication.java
@@ -10,8 +10,9 @@ package com.study.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MallAdminApplication {
 
